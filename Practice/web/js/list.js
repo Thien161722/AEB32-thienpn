5acd5bb9a71d856bb0db42018ff8e80b764f0f6f
Thien161722 <pnhatthien@gmail.com>
2022-12-19 22:51:38 +0700

    practice session 09

--- Practice/web/js/list.js
+++ Practice/web/js/list.js
@@ -1,11 +1,3 @@
-// console.log(1);
-
-let user = {
-  id: 1,
-  name: "nam",
-  age: 30,
-  address: "address1",
-};
 
 const listUser = [
   {
@@ -15,28 +7,32 @@ const listUser = [
     address: "address1",
   },
   {
-    id: 1,
-    name: "name1",
-    age: 30,
-    address: "address1",
+    id: 2,
+    name: "name2",
+    age: 35,
+    address: "address2",
   },
+  {
+    id: 3,
+    name: "name3",
+    age: 37,
+    address: "address3",
+  }
 ];
 let eleBodyTable = document.getElementById("tbody__user");
-
 // console.log(eleBodyTable);
 let resRow = "";
 for (let index = 0; index < listUser.length; index++) {
-  resRow += formatRowUser(user);
-  
+  resRow += formatRowUser(listUser[index]);
 }
-eleBodyTable.innerHTML(resRow);
-
 
 function formatRowUser(_user) {
   return `<tr>
-    <td>${user.id}</td>
-    <td>${user.name}</td>
-    <td>${user.age}</td>
-    <td>${user.address}</td>
+    <td>${_user.id}</td>
+    <td>${_user.name}</td>
+    <td>${_user.age}</td>
+    <td>${_user.address}</td>
   </tr>`;
 }
+eleBodyTable.innerHTML = resRow;
+
